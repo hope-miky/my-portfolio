6d3ab9762dca8933c5ddebd9755f93fcd34b7e0c
Molla.Al <hope.miky1074@gmail.com>
2022-08-21 19:05:25 +0200

    added contacts page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import LandingPageView from "../components/landing_page/LandingPage"
 import NavBarView from "../components/NavBar"
 import ProjectsView from "../components/projects/Projects"
 import ExperiencePageView from "../components/work/Experience"
+import ContactsPageView from "../components/contacts/contacts"
+
 
 const IndexPage = () => {
 
@@ -16,6 +18,7 @@ const IndexPage = () => {
             <AboutMeView />
             <ExperiencePageView />
             <ProjectsView />
+            <ContactsPageView />
         </div>
   
 }
