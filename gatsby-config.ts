d48c7f2c9f21b252fb30a896009781dee1c5d73e
Molla.Al <hope.miky1074@gmail.com>
2022-08-27 12:31:21 +0200

    updated seo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -15,6 +15,7 @@ const config: GatsbyConfig = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
